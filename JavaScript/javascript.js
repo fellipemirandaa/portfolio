3cc3f0032a678cf4e615bc2390cb4c124e2decc0
Fellipe Miranda <fe.miranda1990@gmail.com>
2022-01-28 19:41:46 -0300

    second commit

---
+++ JavaScript/javascript.js
@@ -0,0 +1,8 @@
+function openNav() {
+    document.getElementById("mySidepanel").style.width = "200px"; /* aumenta ou diminui 
+    o tamanho da barra do painel lateral */
+}
+function closeNav() {
+    document.getElementById("mySidepanel").style.width = "0"; /* faz com que o painel lateral 
+    desapareça quando fechar no "x" */
+}
